6857b1cdde9d3287a2b7724ffc8779595d1d18c5
toel-kamanda34 <toelokemwa@gmail.com>
2024-08-12 00:21:24 +0300

    Added event listners to DOM elements

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
+import React from 'react';
 
 function Keypad (){
+    const handleInputChange = (event) => {
+        console.log('Entering password...');
+    };
+
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={handleInputChange} />
+        </div>
     )
 }
 
